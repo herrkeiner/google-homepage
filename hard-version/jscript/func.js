a502d0a38338da6b818c8bf4d53a2b7d5f9622a5
Guilherme Luís Dall'Agnol <gui.logicalt@gmail.com>
2018-07-15 20:16:37 -0300

    Add Settings Dropdown menu and and styling the Tools' button.

--- hard-version/jscript/func.js
+++ hard-version/jscript/func.js
@@ -17,8 +17,25 @@ function showMore() {
     document.getElementById('more-menu-section').style.display = 'block';
 }
 
-function showMoreDropDown() {
-  var e = document.getElementById('sfilter-mdpul');
+function showMoreDropdown() {
+  var e = document.getElementById('sfilter-mdpul'),
+      s = document.getElementById('sfilter-sdpul');
+
+  if (s.style.display === 'block')
+    s.style.display = 'none';
+
+  if (e.style.display === 'none')
+    e.style.display = 'block';
+  else e.style.display = 'none';
+
+}
+
+function showSettingsDropdown() {
+  var e = document.getElementById('sfilter-sdpul'),
+      m = document.getElementById('sfilter-mdpul');
+
+  if (m.style.display === 'block')
+    m.style.display = 'none';
 
   if (e.style.display === 'none')
     e.style.display = 'block';
